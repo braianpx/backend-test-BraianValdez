030eb875584970aff96bc0ae8e67a60f703faf8d
braianpx <braivaldez16@gmail.com>
2024-10-31 03:22:31 -0300

    create crud task

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,4 +1,9 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
+import { TaskService } from './task.service';
+import { Task } from './task.entity';
+import { TaskInterface } from './task.interface';
 
 @Controller('task')
-export class TaskController {}
+export class TaskController {
+  constructor(private readonly taskService: TaskService) {}
+}
--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
-
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Task } from './task.entity';
+import { TaskInterface } from './task.interface';
 @Injectable()
-export class TaskService {}
+export class TaskService {
+  constructor(
+    @InjectRepository(Task)
+    private TaskRepository: Repository<Task>,
+    ){} 
+
+    async createTask(task: TaskInterface){
+      const newTask = this.TaskRepository.create(task);
+      await this.TaskRepository.save(newTask)
+      return task;
+    }
+    async getAllTask(){
+      return await this.TaskRepository.find();
+    }
+
+  }
