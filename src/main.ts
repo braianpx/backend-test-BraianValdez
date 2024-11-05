e5d11d53003815e65af000994ea5de8af421d3ef
braianpx <braivaldez16@gmail.com>
2024-11-05 06:47:31 -0300

    update config main

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,10 @@ async function bootstrap() {
     whitelist: true,  // borra las propiedades que no estan definidas en el DTO
     forbidNonWhitelisted: true,  // lanza un error si recibe una propiedad no definida en el DTO
   }));
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  });
   swagger(app) // config swagger
   await app.listen(3000);
   console.log('server listen in', 3000)
